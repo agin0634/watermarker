9767b0cc9ea60d905c5b66455df1a9f08397d174
Duri Shen <agin0511@gmail.com>
2020-01-15 18:08:40 +0800

    finish all features

--- main.py
+++ main.py
@@ -4,35 +4,32 @@ import sys
 if hasattr(sys, 'frozen'):
     os.environ['PATH'] = sys._MEIPASS + ";" + os.environ['PATH']
 from mainUI import Ui_MainWindow
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtWidgets import QDialog, QApplication, QMainWindow, QColorDialog
 
 class App():
     def previewImg(self, imgfile):
         try:
             img = Image.open(imgfile)
-            
-            '''draw = ImageDraw.Draw(img)
-            draw.text(xy=(100,100),text="Hello World",fill=(255,69,0))
-            img.show()'''
-            #img.show()
             return img
         except Exception as e:
             print(e)
             return None
-        
 
     def drawText(self, img, loc, text, color, font, fontsize):
         draw = ImageDraw.Draw(img)
-        cfont = 'C:/Windows/Fonts/'  + font
-        # TODO get font
-        print(cfont)
-        cfont = ImageFont.truetype(cfont,fontsize)
-        draw.text(xy=loc,text=text,fill=color,font=cfont,align='center')
+        fontpath = 'Fonts/'  + font
+        fontpath = ImageFont.truetype(fontpath,fontsize)
+        draw.text(xy=loc,text=text,fill=color,font=fontpath,align='center')
         img.show()
+
+    def fontsBrowser(self):
+        fonts = os.listdir("./fonts")
+        return fonts
 
 class AppWindow(QMainWindow, Ui_MainWindow):
     # parameter
+    img_path = ''
     pre_img = None
     text_loc = (100,0)
     text = ''
@@ -44,62 +41,88 @@ class AppWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.addFontstoComboBox()
+
+    def addFontstoComboBox(self):
+        fonts = App.fontsBrowser(App)
+        for i in range(len(fonts)):
+            f = fonts[i]
+            f = f.replace('.ttf','')
+            self.ui.comboBox_font.addItem(f)
+
+            #TODO font style preview
+            '''
+            item = self.ui.comboBox_font.model().item(i)
+            font = item.font() 
+            font.setPointSize(10) 
+            font.setFamily("Onthehill.ttf")
+            item.setFont(font)
+            '''
+    
+    def eventColorPicker(self):
+        color = QColorDialog.getColor()
+        self.ui.colorButton.setStyleSheet('QWidget {background-color:%s}'%color.name())
+        r = color.red()
+        g = color.green()
+        b = color.blue()
+        self.color = (r,g,b)
 
     def eventBrowseImg(self):
+        App.fontsBrowser(App)
         imgfile = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File',os.getcwd(),"All Files(*jpg *png)")
         if imgfile:
-            print(imgfile[0])
-            self.pre_img = App.previewImg(App, imgfile[0])
-            #color
-            '''
-            graphicscene = QtWidgets.QGraphicsScene()
-            graphicscene.addWidget(pre_img)
-            self.ui.graphicsView.setScene(graphicscene)
-            self.ui.graphicsView.show()
-            '''
+            print(imgfile)
+            self.img_path = imgfile[0]
+            self.pre_img = App.previewImg(App,self.img_path)
+        else:
+            pass
             
     def eventSaveImg(self):
-        out = self.pre_img
-        out = out.convert("RGB")
-        out.save('img.jpg', quality=90, subsampling=0)
-        print('save')
+        if self.pre_img != None:
+            try:
+                filename = 'wm_' + os.path.basename(self.img_path)
+                out = self.pre_img
+                out = out.convert("RGB")
+                out.save(filename, quality=90, subsampling=0)
+                print('save')
+            except Exception as e:
+                print(e)
+                pass
+        else:
+            pass
 
     def eventPreview(self):
         if self.ui.lineEdit.text():
             self.text = self.ui.lineEdit.text()
-        self.text_font = self.fontStyle()
-        self.text_size = int(self.ui.comboBox_fontsize.currentText())
 
-        App.drawText(App,self.pre_img,self.text_loc,self.text,self.color,self.text_font,self.text_size)
+        if self.pre_img != None:
+            i = App.previewImg(App,self.img_path)
+            self.text_font = self.fontStyle()
+            self.text_size = int(self.ui.comboBox_fontsize.currentText())
+            currenttext_loc = self.addOffset(self.text_loc)
+            try:
+                App.drawText(App,i,currenttext_loc,self.text,self.color,self.text_font,self.text_size)
+                self.pre_img = i
+            except Exception as e:
+                print(e)
+                pass
+        else:
+            pass
 
-    def addOffset(self):
+    def addOffset(self, text_loc):
+        w, h = text_loc
         up = self.ui.spinBox_up.text()
         down = self.ui.spinBox_down.text()
         left = self.ui.spinBox_left.text()
         right = self.ui.spinBox_right.text()
         x = int(right) - int(left)
         y = int(down) - int(up)
-        pass
-
-    # -- fonts style events
-    bisBold = False
+        text_loc = (w + x, h + y)
+        return text_loc
 
     def fontStyle(self):
-        font = self.ui.ComboBox_font.currentText() +'.ttf'
-        print(font)
+        font = self.ui.comboBox_font.currentText() +'.ttf'
         return font
-
-    def eventBold(self):
-        self.fontStyle()
-        if self.bisBold:
-            self.bisBold = False
-            self.ui.boldButton.setStyleSheet('color: rgb(220, 220, 220);\nbackground-color: rgb(98, 98, 115);')
-        else:
-            self.bisBold = True
-            self.ui.boldButton.setStyleSheet('color: rgb(0, 0, 0);\nbackground-color: rgb(249, 201, 91);')
-
-
-        
 
     # -- align events
     def eventAlign00(self):
